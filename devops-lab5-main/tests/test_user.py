d7554576dc797d604388cfec1d00eb47a598278b
unknown <razzhivin546@gmail.com>
2025-04-12 22:19:48 +0300

    second

--- devops-lab5-main/tests/test_user.py
+++ devops-lab5-main/tests/test_user.py
@@ -1,10 +1,8 @@
 from fastapi.testclient import TestClient
-
 from src.main import app
 
 client = TestClient(app)
 
-# Существующие пользователи
 users = [
     {
         'id': 1,
@@ -19,23 +17,50 @@ users = [
 ]
 
 def test_get_existed_user():
-    '''Получение существующего пользователя'''
     response = client.get("/api/v1/user", params={'email': users[0]['email']})
     assert response.status_code == 200
     assert response.json() == users[0]
 
 def test_get_unexisted_user():
-    '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': "nonexistent@mail.com"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
 
 def test_create_user_with_valid_email():
-    '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        'id': 3,
+        'name': 'Sergey Sergeev',
+        'email': 's.s.sergeev@mail.com'
+    }
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+    assert response.json() == new_user
+    
+    response = client.get("/api/v1/user", params={'email': new_user['email']})
+    assert response.status_code == 200
+    assert response.json() == new_user
 
 def test_create_user_with_invalid_email():
-    '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    duplicate_user = {
+        'id': 4,
+        'name': 'Another Ivan',
+        'email': users[0]['email']
+    }
+    response = client.post("/api/v1/user", json=duplicate_user)
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Email already registered"}
 
 def test_delete_user():
-    '''Удаление пользователя'''
-    pass
+    temp_user = {
+        'id': 5,
+        'name': 'To Delete',
+        'email': 'to.delete@mail.com'
+    }
+    client.post("/api/v1/user", json=temp_user)
+    
+    response = client.delete(f"/api/v1/user/{temp_user['id']}")
+    assert response.status_code == 200
+    assert response.json() == {"message": "User deleted successfully"}
+    
+    response = client.get("/api/v1/user", params={'email': temp_user['email']})
+    assert response.status_code == 404
